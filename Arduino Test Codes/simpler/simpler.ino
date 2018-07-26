06617fa35ceeda59e3b73fabc87882146da8fbf2
Tarun <trmittal24@gmail.com>
2018-07-26 19:52:31 +0530

    First

---
+++ Arduino Test Codes/simpler/simpler.ino
@@ -0,0 +1,66 @@
+#define NUM 0
+inline void thamba()
+{
+  asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );asm ( "nop \n" );    
+}
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  // disable ADC
+  ADCSRA = 0; // 
+  PRR = 0; // disable peripherals: timers, etc.
+  // pervent power spikes at every 1 ms intervals
+  SREG &= ~0x80 ;
+
+  Serial.begin(115200);
+}
+
+uint8_t i=1;
+uint8_t j=99;
+uint8_t a = 10;
+uint8_t c;
+uint8_t d,e,f;
+uint8_t k=11;
+uint8_t x=25;
+void loop() {
+  // put your main code here, to run repeatedly:
+  d = 10;
+  while(1)
+  {
+ 
+   
+    x = 0x1a;
+    PORTB ^= 0x10 ;  // same as digitalWRITE(12, ...) BUT LOWER POWER USAGE
+   for(k=0; k<8;k++)
+   {
+    PORTB ^= 0x08 ;  // same as digitalWRITE(11, ...) BUT LOWER POWER USAGE
+    if(x % 2 == 0 )
+    {
+      thamba();
+      c = d + 1; 
+      thamba();
+    }
+    else
+    {
+      thamba();
+      c = d * j;
+      thamba();
+    }
+    thamba();
+    UDR0 = c;
+    thamba();
+    x = x >> 1;
+    thamba();
+   }
+   //digitalWrite(12,0);  
+  }
+  //c = c * a ; 
+  //c = c * a ; 
+  //c = c * a ; 
+//  c = c * a ; 
+//  c = c * a ;  
+  //delay(1);
+  
+}
