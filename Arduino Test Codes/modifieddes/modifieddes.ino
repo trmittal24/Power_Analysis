cfc8b6481603c56e2067365f09ac7a75b6608d51
Vishal Chandratreya <vpaijc@gmail.com>
2018-09-02 10:41:32 +0530

    made comments concise, used block comment

--- Arduino Test Codes/modifieddes/modifieddes.ino
+++ Arduino Test Codes/modifieddes/modifieddes.ino
@@ -1,10 +1,9 @@
 void setup() {
-  // put your setup code here, to run once:
-   Serial.begin(115200);
+   Serial.begin(115200); // maximum baud rate
    pinMode(11, OUTPUT);
    pinMode(12, OUTPUT);
 
-   SREG &= ~0x80 ; // pervent power spikes at every 1 ms intervals
+   SREG &= ~0x80; // pervent power spikes at every 1 ms intervals
 }
 uint8_t state = 78;
 uint8_t key = 13;
@@ -12,25 +11,20 @@ uint8_t left; // more significant 4 bits of state
 uint8_t right; // less significant 4 bits of state
 uint8_t swap; // the swapper variable
 uint8_t ct, dct;
-// counter variable
 int count;
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-
-  
+void loop() {  
   // split the plaintext
   left = state >> 4;
   right = state % 16;
+  
+  // reference signal to indicate start of loop
   PORTB &= ~0x10;
-
-  // encode
+  
+  // loop
   for(count = 0; count < 4; count++)
   {
-    // show the current state
-    //PORTB |= 0x08;
-    // XOR them only if LSB of key is high
+    // operation performed only if key is odd
     if(key % 2)
     {
       swap = left;
@@ -39,38 +33,38 @@ void loop() {
       right ^= key;
       left ^= right;
     }
-    //PORTB &= ~0x08;
+    
     // rotate key right
     key = ((key >> 1) + (key << 3)) % 16;
-    //swap = count % 8;
-    //PORTB &= ~0x08;
   }
-
+  
+  // reference signal to indicate end of loop
   PORTB |= 0x10;
   
+  // obtain encrypted value and send it
   ct = (left << 4) + right;
-//  
   UDR0 = ct;
-//  
-//  for(count = 0; count < 4; count++)
-//  {
-//    // show the current state
-//
-//    // rotate key left
-//    key = ((key >> 3) + (key << 1)) % 16;
-//
-//    // undo the effects of XOR only if LSB of key is high
-//    if(key % 2)
-//    {
-//      right ^= key;
-//      left ^= key ^ right;
-//      swap = left;
-//      left = right;
-//      right = swap;
-//    }
-//  }
-//  dct = (left << 4) + right;
-//  if(dct == state){ 
-//    PORTB |= 0x10;
-//  }
+  
+  // decoding
+  /*
+  for(count = 0; count < 4; count++)
+  {
+    // rotate key left
+    key = ((key >> 3) + (key << 1)) % 16;
+
+    // undo the effects of XOR only if LSB of key is high
+    if(key % 2)
+    {
+      right ^= key;
+      left ^= key ^ right;
+      swap = left;
+      left = right;
+      right = swap;
+    }
+  }
+  dct = (left << 4) + right;
+  if(dct == state){ 
+    PORTB |= 0x10;
+  }
+  */
 }
