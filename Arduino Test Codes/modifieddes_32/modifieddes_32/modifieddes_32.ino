9b33e4a4723fbbd38c48b4ae46541c0e3ccc9474
Tarun <trmittal24@gmail.com>
2018-09-04 15:20:34 +0530

    32 bit DES

--- Arduino Test Codes/modifieddes_32/modifieddes_32/modifieddes_32.ino
+++ Arduino Test Codes/modifieddes_32/modifieddes_32/modifieddes_32.ino
@@ -5,24 +5,24 @@ void setup() {
 
    SREG &= ~0x80; // pervent power spikes at every 1 ms intervals
 }
-unsigned int state = 78;
-unsigned int key = 36 ;
-unsigned int left; // more significant 4 bits of state
-unsigned int right; // less significant 4 bits of state
-unsigned int swap; // the swapper variable
-unsigned int ct, dct;
+unsigned long int state = 78;
+unsigned long int key = 42069;
+unsigned long int left; // more significant 4 bits of state
+unsigned long int right; // less significant 4 bits of state
+unsigned long int swap; // the swapper variable
+unsigned long int ct, dct;
 int count;
 
 void loop() {  
   // split the plaintext
-  left = state >> 8;
-  right = state % 256;
+  left = state >> 16;
+  right = state % 65536;
   
   // reference signal to indicate start of loop
   PORTB &= ~0x10;
   
   // loop
-  for(count = 0; count < 8; count++)
+  for(count = 0; count < 16; count++)
   {
     //PORTB |= 0x08;
     // operation performed only if key is odd
@@ -37,7 +37,7 @@ void loop() {
 
     //PORTB &= ~0x08;
     // rotate key right
-    key = ((key >> 1) + (key << 7)) % 256;
+    key = ((key >> 1) + (key << 15)) % 65536;
   }
   
   // reference signal to indicate end of loop
