559d02df35ec09acf563f925cce6a2b2615c4d70
34point5 <vpaijc@gmail.com>
2019-01-10 16:30:51 +0530

    corrected modular exponentiation program

--- Arduino Test Codes/mod_expo/mod_expo.ino
+++ Arduino Test Codes/mod_expo/mod_expo.ino
@@ -9,41 +9,90 @@
  * 
  */
 
+////////////////////////////////////////////////////////////////////////////////
 
-long int expo, num, modn, r;
+void print64(uint64_t num)
+{
+  char rev[128]; 
+  char *p = rev+1;
+  while (num)
+  {
+    *p++ = '0' + num % 10;
+    num /= 10;
+  }
+  p--;
+  while(p > rev)
+  {
+    Serial.print(*p--);
+  }
+  Serial.println();
+}
 
-void setup() {
-  // put your setup code here, to run once:
-  modn = 3233;
-  //expo = 0b1011100010;
-  num = 123;
-  // should get back r = 855
+////////////////////////////////////////////////////////////////////////////////
+
+uint64_t n, p, q, M, e, C, x;
+
+////////////////////////////////////////////////////////////////////////////////
+
+void setup()
+{
+  p = 23831;
+  q = 65657;
+  n = p * q;
+  M = 65;
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
-  //Serial.begin(9600);
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
-  r = 1;
-  expo = 0b10111001;
-  PORTB &= ~0x10;
-  while(expo > 0)
-  {
-    //PORTB &= ~0x08;
-    
-    r = (r*r) % modn;
 
-    if((expo % 2) == 1)
-    {
-      r = (r*num) % modn;
-    }
 
-    expo = expo >> 1;
-    //PORTB |= 0x08;
-  }
   
-  PORTB |= 0x10;
+//  e = 4428079;
+//  modn = 3233;
+//  //expo = 0b1011100010;
+//  num = 123;
+//  // should get back r = 855
+  Serial.begin(9600);
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
+void loop()
+{
+  e = 3427;
+  C = 1;
+  x = M;
+  while(e > 0)
+  {
+    
+    if(e % 2)
+    {
+      C = (C * x) % n;
+    }
+    print64(C);
+    e >>= 1;
+    x = (x * x) % n;
+  }
+  print64(C);
+  delay(5000);
+
+//  r = 1;
+//  expo = 0b10111001;
+//  PORTB &= ~0x10;
+//  while(expo > 0)
+//  {
+//    //PORTB &= ~0x08;
+//    
+//    r = (r*r) % modn;
+//
+//    if((expo % 2) == 1)
+//    {
+//      r = (r*num) % modn;
+//    }
+//
+//    expo = expo >> 1;
+//    //PORTB |= 0x08;
+//  }
+//  
+//  PORTB |= 0x10;
+}
