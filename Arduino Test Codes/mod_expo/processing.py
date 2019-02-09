5cd30f24efb7fd23be4fdc98f1cbf1e923124579
34point5 <vpaijc@gmail.com>
2019-02-10 00:45:35 +0530

    code formatting

--- Arduino Test Codes/mod_expo/processing.py
+++ Arduino Test Codes/mod_expo/processing.py
@@ -32,7 +32,7 @@ def savitzky_golay(y, window_size, order, deriv = 0, rate = 1):
 
 	# set up
 	order_range = range(order + 1)
-	half_window = (window_size -1) // 2
+	half_window = (window_size - 1) // 2
 	b = np.mat([[k ** i for i in order_range] for k in range(-half_window, half_window + 1)])
 	m = np.linalg.pinv(b).A[deriv] * rate ** deriv * factorial(deriv)
 	firstvals = y[0] - np.abs(y[1 : half_window + 1][:: -1] - y[0])
